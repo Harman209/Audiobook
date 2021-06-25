5f15b088295b95a586f1891435b945f1cfb8fb23
Harman Kajla <harmankajla209@gmail.com>
2021-06-25 13:30:44 +0530

    Upadte V1

---
+++ main.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+import PyPDF2
+import pdfplumber
+
+file = input("Name of the file: \n")
+pdfFileObj = open(file, 'rb')
+pdfeader = PyPDF2.PdfFileReader(pdfFileObj)
+pages = pdfeader.numPages
+language = 'en'
+start = input('Enter the starting page no.: ') - 1
+
+with pdfplumber.open(file) as pdf:
+    for i in range(start, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        print(text)
+
+def speak(text):
+    myobj = gTTS(text=text, lang=language, slow=False)
+    myobj.save('audiobook.mp3')
+
+speak(text)
+
+
+
